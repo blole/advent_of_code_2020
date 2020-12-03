87a8707529eded398756a4a7bc9070e83f81f5d6
Björn Holm <blollle@gmail.com>
2020-12-03 18:24:20 +0100

    +Kattio.line()

--- src/main/kotlin/Kattio.kt
+++ src/main/kotlin/Kattio.kt
@@ -5,43 +5,45 @@ import java.util.*
 
 class Kattio(i: InputStream = System.`in`, o: OutputStream = System.out) : PrintWriter(BufferedOutputStream(o)) {
     private val reader: BufferedReader = BufferedReader(InputStreamReader(i))
+
     private var tokenizer: StringTokenizer? = null
-    private var token: String? = null
-
-    private fun peekToken(): String? {
-        if (token == null) {
-            try {
-                while (tokenizer == null || !tokenizer!!.hasMoreTokens()) {
-                    var line = reader.readLine() ?: return null
-                    tokenizer = StringTokenizer(line)
-                }
-                token = tokenizer!!.nextToken()
-            } catch (e: IOException) {
+        get() {
+            while (field == null || !field!!.hasMoreTokens()) {
+                val line = reader.readLine() ?: return null
+                field = StringTokenizer(line)
             }
+            return field
         }
-        return token
-    }
 
-    private fun nextTokenOrNull(): String? = peekToken().also { token = null }
-    private fun nextToken() = nextTokenOrNull()!!
+    private var token: String? = null
+        get() {
+            if (field == null)
+                field = tokenizer?.nextToken()
+            return field
+        }
 
-    val hasMoreTokens: Boolean get() = peekToken() != null
+    private fun nextTokenOrNull(): String? = token.also { token = null }
 
-    fun int():    Int        = nextToken().toInt()
-    fun long():   Long       = nextToken().toLong()
-    fun bigint(): BigInteger = nextToken().toBigInteger()
-    fun double(): Double     = nextToken().toDouble()
-    fun word():   String     = nextToken()
+    val hasMoreTokens: Boolean get() = token != null
 
     fun intOrNull():    Int?        = nextTokenOrNull()?.toInt()
     fun longOrNull():   Long?       = nextTokenOrNull()?.toLong()
     fun bigintOrNull(): BigInteger? = nextTokenOrNull()?.toBigInteger()
     fun doubleOrNull(): Double?     = nextTokenOrNull()?.toDouble()
     fun wordOrNull():   String?     = nextTokenOrNull()
+    fun lineOrNull():   String?     = tokenizer?.nextToken("")?.also { token = null }
 
-    fun ints():    Sequence<Int>        = generateSequence { this.intOrNull() }
-    fun longs():   Sequence<Long>       = generateSequence { this.longOrNull() }
-    fun bigints(): Sequence<BigInteger> = generateSequence { this.bigintOrNull() }
-    fun doubles(): Sequence<Double>     = generateSequence { this.doubleOrNull() }
-    fun words():   Sequence<String>     = generateSequence { this.wordOrNull() }
+    fun int():    Int        = intOrNull()!!
+    fun long():   Long       = longOrNull()!!
+    fun bigint(): BigInteger = bigintOrNull()!!
+    fun double(): Double     = doubleOrNull()!!
+    fun word():   String     = wordOrNull()!!
+    fun line():   String     = lineOrNull()!!
+
+    fun ints():    Sequence<Int>        = generateSequence { intOrNull() }
+    fun longs():   Sequence<Long>       = generateSequence { longOrNull() }
+    fun bigints(): Sequence<BigInteger> = generateSequence { bigintOrNull() }
+    fun doubles(): Sequence<Double>     = generateSequence { doubleOrNull() }
+    fun words():   Sequence<String>     = generateSequence { wordOrNull() }
+    fun lines():   Sequence<String>     = generateSequence { lineOrNull() }
 }
