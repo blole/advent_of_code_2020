3b163b132f938e84324bb62f48a889122339612a
Björn Holm <blollle@gmail.com>
2020-12-05 21:42:16 +0100

    new Kattio...

--- src/main/kotlin/Kattio.kt
+++ src/main/kotlin/Kattio.kt
@@ -1,37 +1,84 @@
+import Tokenizer.Companion.tokenize
 import java.io.*
 import java.math.BigInteger
-import java.util.*
 
+class Tokenizer(val line: String) {
+    private var start = 0
+
+    private fun nextTokenRange(delimiters: String? = null): IntRange? {
+
+        var i = start
+
+        if (delimiters == null) {
+            while (i < line.length && line[i].isWhitespace()) i++
+        } else {
+            while (i < line.length && line[i] in delimiters) i++
+        }
+
+        val from = i
+
+        if (delimiters == null) {
+            while (i < line.length && !line[i].isWhitespace()) i++
+        } else {
+            while (i < line.length && line[i] !in delimiters) i++
+        }
+
+        return (from until i).takeIf { !it.isEmpty() }
+    }
+
+    fun hasMoreTokens(delimiters: String? = null): Boolean = nextTokenRange(delimiters) != null
+
+    fun peekToken(delimiters: String? = null): String? {
+        if (start == 0 && delimiters == "" && line.length == 0)
+            return "".also { start = 1 }
+
+        val nextTokenRange = nextTokenRange(delimiters) ?: return null
+        return line.substring(nextTokenRange)
+    }
+
+    fun nextToken(delimiters: String? = null): String? {
+        if (start == 0 && delimiters == "" && line.length == 0)
+            return "".also { start = 1 }
+
+        val nextTokenRange = nextTokenRange(delimiters) ?: return null
+        start = nextTokenRange.last + 1
+        return line.substring(nextTokenRange)
+    }
+
+    companion object {
+        fun String.tokenize(): Tokenizer = Tokenizer(this)
+    }
+}
 
 class Kattio(i: InputStream = System.`in`, o: OutputStream = System.out) : PrintWriter(BufferedOutputStream(o)) {
     private val reader: BufferedReader = BufferedReader(InputStreamReader(i))
 
-    private var tokenizer: StringTokenizer? = null
-        get() {
-            while (field == null || !field!!.hasMoreTokens()) {
-                val line = reader.readLine() ?: return null
-                field = StringTokenizer(line)
-            }
-            return field
-        }
-
-    private var token: String? = null
+    private var tokenizer: Tokenizer? = null
         get() {
             if (field == null)
-                field = tokenizer?.nextToken()
+                field = reader.readLine()?.tokenize()
             return field
         }
 
-    private fun nextTokenOrNull(): String? = token.also { token = null }
+    private fun nextToken(delimiters: String? = null): String? {
+        while (tokenizer != null) {
+            val token = tokenizer!!.nextToken(delimiters)
+            if (token != null)
+                return token
+            else
+                tokenizer = null
+        }
+        return null
+    }
 
-    val hasMoreTokens: Boolean get() = token != null
+    val hasMoreTokens: Boolean get() = tokenizer?.hasMoreTokens() ?: false
 
-    fun intOrNull():    Int?        = nextTokenOrNull()?.toInt()
-    fun longOrNull():   Long?       = nextTokenOrNull()?.toLong()
-    fun bigintOrNull(): BigInteger? = nextTokenOrNull()?.toBigInteger()
-    fun doubleOrNull(): Double?     = nextTokenOrNull()?.toDouble()
-    fun wordOrNull():   String?     = nextTokenOrNull()
-    fun lineOrNull():   String?     = tokenizer?.nextToken("")?.also { token = null }
+    fun intOrNull():    Int?        = nextToken()?.toInt()
+    fun longOrNull():   Long?       = nextToken()?.toLong()
+    fun bigintOrNull(): BigInteger? = nextToken()?.toBigInteger()
+    fun doubleOrNull(): Double?     = nextToken()?.toDouble()
+    fun wordOrNull():   String?     = nextToken()
+    fun lineOrNull():   String?     = nextToken("")
 
     fun int():    Int        = intOrNull()!!
     fun long():   Long       = longOrNull()!!
