3bfff0918e4e93927c9617c62ecdee14e2b3764e
Björn Holm <blollle@gmail.com>
2020-12-07 22:32:58 +0100

    day06 plus -> union

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -2,9 +2,10 @@ class Day06(io: Kattio) {
     val answerGroups = io.lines()
         .joinToString(" ")
         .split("  ")
-        .map { group -> group.split(" ").map { it.toSet() } }
+        .map { group ->
+            group.split(" ").map { it.toSet() }
+        }
 
-    fun a() = answerGroups.sumBy { it.reduce(Set<Char>::plus).size }
-
+    fun a() = answerGroups.sumBy { it.reduce(Set<Char>::union).size }
     fun b() = answerGroups.sumBy { it.reduce(Set<Char>::intersect).size }
 }
