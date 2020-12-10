6814dfa759f0ad17a9ec6e67cadfbf83e1049414
Björn Holm <blollle@gmail.com>
2020-12-10 09:32:02 +0100

    day09 toHashSet()

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -2,7 +2,7 @@ class Day09(io: Kattio, val windowSize: Int) {
     private val data = io.longs().toList()
 
     fun List<Long>.hasPairSum(pairSum: Long): Boolean {
-        val set = toSet()
+        val set = toHashSet()
         return set.any { pairSum - it in set }
     }
 
@@ -18,11 +18,12 @@ class Day09(io: Kattio, val windowSize: Int) {
     fun b(): Long {
         val aResult = a()
         val cumsum = data.runningReduce(Long::plus)
+        val cumsumSet = cumsum.toHashSet()
         val cumsumIndex = cumsum.withIndex().map { it.value to it.index }.toMap()
 
         return cumsum
             .single {
-                it - aResult in cumsum && cumsumIndex[it-aResult]!! + 1 < cumsumIndex[it]!!
+                it - aResult in cumsumSet && cumsumIndex[it-aResult]!! + 1 < cumsumIndex[it]!!
             }
             .let {
                 val inRange = data.slice(cumsumIndex[it-aResult]!! + 1 .. cumsumIndex[it]!!)
