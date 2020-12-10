d8813bfde6111715f38a5f6e1495bd136ae13ce5
Björn Holm <blollle@gmail.com>
2020-12-10 01:02:17 +0100

    day09

---
+++ src/main/kotlin/Day09.kt
@@ -0,0 +1,32 @@
+class Day09(io: Kattio, val windowSize: Int) {
+    private val data = io.longs().toList()
+
+    fun List<Long>.hasPairSum(pairSum: Long): Boolean {
+        val set = toSet()
+        return set.any { pairSum - it in set }
+    }
+
+    fun a() = data
+        .windowed(windowSize+1)
+        .first { window ->
+            !window
+                .take(windowSize)
+                .hasPairSum(window.last())
+        }
+        .last()
+
+    fun b(): Long {
+        val aResult = a()
+        val cumsum = data.runningReduce(Long::plus)
+        val cumsumIndex = cumsum.withIndex().map { it.value to it.index }.toMap()
+
+        return cumsum
+            .single {
+                it - aResult in cumsum && cumsumIndex[it-aResult]!! + 1 < cumsumIndex[it]!!
+            }
+            .let {
+                val inRange = data.slice(cumsumIndex[it-aResult]!! + 1 .. cumsumIndex[it]!!)
+                inRange.minOrNull()!! + inRange.maxOrNull()!!
+            }
+    }
+}
--- src/test/kotlin/AocTests.kt
+++ src/test/kotlin/AocTests.kt
@@ -178,4 +178,34 @@ class AocTests {
         assertEquals(8, Day08(string(example)).b())
         assertEquals(631, Day08(file(8)).b())
     }
+
+    @Test
+    fun day09() {
+        val example = """
+            35
+            20
+            15
+            25
+            47
+            40
+            62
+            55
+            65
+            95
+            102
+            117
+            150
+            182
+            127
+            219
+            299
+            277
+            309
+            576
+        """.trimIndent()
+        assertEquals(127, Day09(string(example), 5).a())
+        assertEquals(1398413738, Day09(file(9), 25).a())
+        assertEquals(62, Day09(string(example), 5).b())
+        assertEquals(169521051, Day09(file(9), 25).b())
+    }
 }
